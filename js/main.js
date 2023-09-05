d8f5c41dc0deec91bcafde04a8358b7ea140d7b8
ansourian <jairansourian@gmail.com>
2023-09-05 20:27:24 -0300

    transiciones fade-in

--- js/main.js
+++ js/main.js
@@ -9,3 +9,33 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  function checkSections() {
+    const sections = document.querySelectorAll('.fade-section');
+  
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      const isVisible = (rect.top <= window.innerHeight * 0.75 && rect.bottom >= 0);
+  
+      if (isVisible) {
+        section.classList.add('visible');
+      } else {
+        section.classList.remove('visible');
+      }
+    });
+  }
+  
+  // Llama a checkSections al cargar la página
+  window.addEventListener('load', checkSections);
+  // Llama a checkSections cuando se hace scroll
+  window.addEventListener('scroll', checkSections);
