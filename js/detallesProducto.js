23fd4719271ece73b064a5133cf823b8acb533de
ansourian <jairansourian@gmail.com>
2023-09-11 21:09:51 -0300

    variables modelos

--- js/detallesProducto.js
+++ js/detallesProducto.js
@@ -122,7 +122,358 @@ document.addEventListener('DOMContentLoaded', function () {
             tipoCuero: 'Cuero vacuno',
             imagenPrincipal: "../assets/img/ready to wear/121 - 8.jpg",
             imagenes: ["../assets/img/ready to wear/121 - 8.jpg", "../assets/img/ready to wear/121 - 9.jpg", "../assets/img/ready to wear/121 - 10.jpg", "../assets/img/ready to wear/121 - 11.jpg"]
-        }        
+        },
+        {
+            id: 'Oxford-Maiz',
+            titulo: 'Oxford (Maíz)',
+            estilo: 'READY TO WEAR',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Azul',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/ready to wear/122 - 29.jpg",
+            imagenes: ["../assets/img/ready to wear/122 - 29.jpg", "../assets/img/ready to wear/119 - 18.jpg", "../assets/img/ready to wear/119 - 19.jpg", "../assets/img/ready to wear/119 - 20.jpg"]
+        },
+        {
+            id: 'Oxford-Verde',
+            titulo: 'Oxford (Verde)',
+            estilo: 'READY TO WEAR',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Negro',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/ready to wear/123 - 1.jpg",
+            imagenes: ["../assets/img/ready to wear/123 - 1.jpg", "../assets/img/ready to wear/119 - 22.jpg", "../assets/img/ready to wear/119 - 23.jpg", "../assets/img/ready to wear/119 - 24.jpg"]
+        },
+        {
+            id: 'Oxford-Natural',
+            titulo: 'Oxford (Natural)',
+            estilo: 'READY TO WEAR',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Negro',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/ready to wear/123 - 5.jpg",
+            imagenes: ["../assets/img/ready to wear/123 - 5.jpg", "../assets/img/ready to wear/121 - 2.jpg", "../assets/img/ready to wear/121 - 3.jpg", "../assets/img/ready to wear/121 - 4.jpg"]
+        },
+        {
+            id: 'Oxford-Chocolate',
+            titulo: 'Oxford (Chocolate)',
+            estilo: 'READY TO WEAR',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Chocolate',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/ready to wear/123 - 9.jpg",
+            imagenes: ["../assets/img/ready to wear/123 - 9.jpg", "../assets/img/ready to wear/121 - 5.jpg", "../assets/img/ready to wear/121 - 6.jpg", "../assets/img/ready to wear/121 - 7.jpg"]
+        },
+        {
+            id: 'Mocasin-IOAN',
+            titulo: 'Mocasín IOAN',
+            estilo: 'READY TO WEAR',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Marrón',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/ready to wear/123 - 32.jpg",
+            imagenes: ["../assets/img/ready to wear/123 - 32.jpg", "../assets/img/ready to wear/121 - 9.jpg", "../assets/img/ready to wear/121 - 10.jpg", "../assets/img/ready to wear/121 - 11.jpg"]
+        },
+        {
+            id: 'Swann-Maiz',
+            titulo: 'Swann (Maíz)',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Maíz',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/made by order/112 - 21.jpg",
+            imagenes: ["../assets/img/made by order/112 - 21.jpg", "../assets/img/made by order/112 - 22.jpg", "../assets/img/made by order/112 - 23.jpg", "../assets/img/made by order/112 - 24.jpg"]
+        },
+        {
+            id: 'Swann-Marron',
+            titulo: 'Swann (Marrón)',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Marrón',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/made by order/119 - 5.jpg",
+            imagenes: ["../assets/img/made by order/119 - 5.jpg", "../assets/img/made by order/119 - 6.jpg", "../assets/img/made by order/119 - 7.jpg", "../assets/img/made by order/119 - 8.jpg"]
+        },
+        {
+            id: 'Swann-Imperio-Negro',
+            titulo: 'Swann (Imperio Negro)',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Imperio Negro',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/made by order/112 - 13.jpg",
+            imagenes: ["../assets/img/made by order/112 - 13.jpg", "../assets/img/made by order/112 - 14.jpg", "../assets/img/made by order/112 - 15.jpg", "../assets/img/made by order/112 - 16.jpg"]
+        },
+        {
+            id: 'Swann-Bota-Alpina-1101-Negro',
+            titulo: 'Swann Bota Alpina 1101',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Negro',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/made by order/123 - 15.jpg",
+            imagenes: ["../assets/img/made by order/123 - 15.jpg", "../assets/img/made by order/112 - 34.jpg", "../assets/img/made by order/112 - 35.jpg", "../assets/img/made by order/112 - 36.jpg"]
+        },
+        {
+            id: 'Swann-Bota-Alpina-1101-Marron',
+            titulo: 'Swann Bota Alpina 1101',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Marrón',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/made by order/117 - 01.jpg",
+            imagenes: ["../assets/img/made by order/117 - 01.jpg", "../assets/img/made by order/117 - 02.jpg", "../assets/img/made by order/112 - 35.jpg", "../assets/img/made by order/112 - 36.jpg"]
+        },
+        {
+            id: 'Bota-Chelsea-Gamuza-Gris',
+            titulo: 'Bota Chelsea (Gamuza Gris)',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Gris',
+            tipoCuero: 'Gamuza',
+            imagenPrincipal: "../assets/img/made by order/85 - 11.jpg",
+            imagenes: ["../assets/img/made by order/85 - 11.jpg", "../assets/img/made by order/113 - 21.jpg", "../assets/img/made by order/113 - 22.jpg", "../assets/img/made by order/113 - 23.jpg"]
+        },
+        {
+            id: 'Bota-Chelsea-Gamuza-Negro',
+            titulo: 'Bota Chelsea (Gamuza Negro)',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Negro',
+            tipoCuero: 'Gamuza',
+            imagenPrincipal: "../assets/img/made by order/113 - 24.jpg",
+            imagenes: ["../assets/img/made by order/113 - 24.jpg", "../assets/img/made by order/113 - 25.jpg", "../assets/img/made by order/113 - 26.jpg", "../assets/img/made by order/113 - 27.jpg"]
+        },
+        {
+            id: 'Bota-Chelsea-Gamuza-Marron',
+            titulo: 'Bota Chelsea (Gamuza Marrón)',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Marrón',
+            tipoCuero: 'Gamuza',
+            imagenPrincipal: "../assets/img/made by order/114 - 1.jpg",
+            imagenes: ["../assets/img/made by order/114 - 1.jpg", "../assets/img/made by order/114 - 2.jpg", "../assets/img/made by order/114 - 3.jpg", "../assets/img/made by order/113 - 27.jpg"]
+        },
+        {
+            id: 'Bota-Chelsea-Gamuza-Verde',
+            titulo: 'Bota Chelsea (Gamuza Verde)',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Verde',
+            tipoCuero: 'Gamuza',
+            imagenPrincipal: "../assets/img/made by order/117 - 03.jpg",
+            imagenes: ["../assets/img/made by order/117 - 03.jpg", "../assets/img/made by order/117 - 04.jpg", "../assets/img/made by order/117 - 05.jpg", "../assets/img/made by order/113 - 27.jpg"]
+        },
+        {
+            id: 'Oxford-Wingtips',
+            titulo: 'Oxford Wingtips',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Marrón - Maíz',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/made by order/112 - 25.jpg",
+            imagenes: ["../assets/img/made by order/112 - 25.jpg", "../assets/img/made by order/112 - 26.jpg", "../assets/img/made by order/112 - 27.jpg", "../assets/img/made by order/112 - 28.jpg"]
+        },
+        {
+            id: 'Derby-Split-Toe',
+            titulo: 'Derby Split Toe',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Verde - Blanco',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/made by order/113 - 1.jpg",
+            imagenes: ["../assets/img/made by order/113 - 1.jpg", "../assets/img/made by order/113 - 2.jpg", "../assets/img/made by order/113 - 3.jpg", "../assets/img/made by order/113 - 4.jpg"]
+        },
+        {
+            id: 'Escarpin-MP-Chocolate',
+            titulo: 'Escarpin MP (Chocolate)',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Chocolate',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/made by order/112 - 29.jpg",
+            imagenes: ["../assets/img/made by order/112 - 29.jpg", "../assets/img/made by order/112 - 30.jpg", "../assets/img/made by order/112 - 31.jpg", "../assets/img/made by order/112 - 32.jpg"]
+        },
+        {
+            id: 'Escarpin-MP-Verde',
+            titulo: 'Escarpin MP (Verde)',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Verde',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/made by order/119 - 25.jpg",
+            imagenes: ["../assets/img/made by order/119 - 25.jpg", "../assets/img/made by order/119 - 26.jpg", "../assets/img/made by order/119 - 27.jpg", "../assets/img/made by order/119 - 28.jpg"]
+        },
+        {
+            id: 'Escarpin-MP-Maiz',
+            titulo: 'Escarpin MP (Maíz)',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Maíz',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/made by order/119 - 9.jpg",
+            imagenes: ["../assets/img/made by order/119 - 9.jpg", "../assets/img/made by order/119 - 10.jpg", "../assets/img/made by order/119 - 11.jpg", "../assets/img/made by order/119 - 12.jpg"]
+        },
+        {
+            id: 'Borcego-Texano-Natural',
+            titulo: 'Borcego Texano (Natural)',
+            estilo: 'MADE TO ORDER',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Marrón',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/made by order/123 - 12.jpg",
+            imagenes: ["../assets/img/made by order/123 - 12.jpg", "../assets/img/ready to wear/121 - 9.jpg", "../assets/img/ready to wear/121 - 10.jpg", "../assets/img/ready to wear/121 - 11.jpg"]
+        },
+        {
+            id: 'Oxford-Wingtip-Full-Brogued-Maiz',
+            titulo: 'Oxford Wingtip Full Brogued (Maíz)',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Maíz',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/bespoke/112 - 1.jpg",
+            imagenes: ["../assets/img/bespoke/112 - 1.jpg", "../assets/img/bespoke/112 - 2.jpg", "../assets/img/bespoke/112 - 3.jpg", "../assets/img/bespoke/112 - 4.jpg"]
+        },
+        {
+            id: 'Oxford-Wingtip-Full-Brogued-Gamuza-Verde',
+            titulo: 'Oxford Wingtip Full Brogued (Gamuza Verde)',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Verde Inglés',
+            tipoCuero: 'Gamuza',
+            imagenPrincipal: "../assets/img/bespoke/112 - 17.jpg",
+            imagenes: ["../assets/img/bespoke/112 - 17.jpg", "../assets/img/bespoke/112 - 18.jpg", "../assets/img/bespoke/112 - 19.jpg", "../assets/img/bespoke/112 - 20.jpg"]
+        },
+        {
+            id: 'Oxford-Captoe-Semi-Brogued-Marron',
+            titulo: 'Oxford Captoe Semi-Brogued (Marrón)',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Marrón',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/bespoke/112 - 5.jpg",
+            imagenes: ["../assets/img/bespoke/112 - 5.jpg", "../assets/img/bespoke/112 - 6.jpg", "../assets/img/bespoke/112 - 7.jpg", "../assets/img/bespoke/112 - 8.jpg"]
+        },
+        {
+            id: 'Derby-1116-Maiz',
+            titulo: 'Derby 1116 (Maíz)',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Maíz',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/bespoke/113 - 5.jpg",
+            imagenes: ["../assets/img/bespoke/113 - 5.jpg", "../assets/img/bespoke/113 - 6.jpg", "../assets/img/bespoke/113 - 7.jpg", "../assets/img/bespoke/113 - 8.jpg"]
+        },
+        {
+            id: 'Derby-1116-Cognac',
+            titulo: 'Derby 1116 (Cognac)',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Cognac',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/bespoke/113 - 9.jpg",
+            imagenes: ["../assets/img/bespoke/113 - 9.jpg", "../assets/img/bespoke/113 - 10.jpg", "../assets/img/bespoke/113 - 11.jpg", "../assets/img/bespoke/113 - 8.jpg"]
+        },
+        {
+            id: 'Derby-1116-Suela',
+            titulo: 'Derby 1116 (Suela)',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Suela',
+            tipoCuero: 'Cuero vacuno',
+            imagenPrincipal: "../assets/img/bespoke/123 - 20.jpg",
+            imagenes: ["../assets/img/bespoke/123 - 20.jpg", "../assets/img/bespoke/123 - 21.jpg", "../assets/img/bespoke/123 - 22.jpg", "../assets/img/bespoke/113 - 8.jpg"]
+        },
+        {
+            id: 'Swann-Wholecut-Cafe',
+            titulo: 'Swann Wholecut (Café)',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Café',
+            tipoCuero: 'Vacuno',
+            imagenPrincipal: "../assets/img/bespoke/113 - 12.jpg",
+            imagenes: ["../assets/img/bespoke/113 - 12.jpg", "../assets/img/bespoke/113 - 13.jpg", "../assets/img/bespoke/113 - 14.jpg", "../assets/img/bespoke/113 - 15.jpg"]
+        },
+        {
+            id: '1116-Bespoke',
+            titulo: '1116 Bespoke',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Marrón',
+            tipoCuero: 'Vacuno',
+            imagenPrincipal: "../assets/img/bespoke/114 - 19.jpg",
+            imagenes: ["../assets/img/bespoke/114 - 19.jpg", "../assets/img/bespoke/114 - 20.jpg", "../assets/img/bespoke/114 - 21.jpg", "../assets/img/bespoke/114 - 22.jpg"]
+        },
+        {
+            id: 'Borcego-GC-Texano-Negro',
+            titulo: 'Borcego GC Texano',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Negro',
+            tipoCuero: 'Vacuno',
+            imagenPrincipal: "../assets/img/bespoke/115 - 17.jpg",
+            imagenes: ["../assets/img/bespoke/115 - 17.jpg", "../assets/img/bespoke/115 - 18.jpg", "../assets/img/bespoke/115 - 19.jpg", "../assets/img/bespoke/115 - 20.jpg"]
+        },
+        {
+            id: 'Bota-Inglesa-Ñandu',
+            titulo: 'Bota Inglesa',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Natural',
+            tipoCuero: 'Vacuno',
+            imagenPrincipal: "../assets/img/bespoke/115 - 9.jpg",
+            imagenes: ["../assets/img/bespoke/115 - 9.jpg", "../assets/img/bespoke/115 - 10.jpg", "../assets/img/bespoke/115 - 11.jpg", "../assets/img/bespoke/115 - 12.jpg"]
+        },
+        {
+            id: 'Bota-Inglesa-Negro',
+            titulo: 'Bota Inglesa',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Negro',
+            tipoCuero: 'Vacuno',
+            imagenPrincipal: "../assets/img/bespoke/114 - 8.jpg",
+            imagenes: ["../assets/img/bespoke/114 - 8.jpg", "../assets/img/bespoke/114 - 9.jpg", "../assets/img/bespoke/114 - 10.jpg", "../assets/img/bespoke/113 - 19.jpg"]
+        },
+        {
+            id: 'Swann-Museum',
+            titulo: 'Swann (Museum)',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Museum',
+            tipoCuero: 'Vacuno',
+            imagenPrincipal: "../assets/img/bespoke/123 - 16.jpg",
+            imagenes: ["../assets/img/bespoke/123 - 16.jpg", "../assets/img/made by order/113 - 2.jpg", "../assets/img/made by order/113 - 3.jpg", "../assets/img/made by order/113 - 4.jpg"]
+        },
+        {
+            id: 'Untitled',
+            titulo: 'Untitled',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Marrón',
+            tipoCuero: 'Vacuno',
+            imagenPrincipal: "../assets/img/bespoke/123 - 24.jpg",
+            imagenes: ["../assets/img/bespoke/123 - 24.jpg", "../assets/img/bespoke/123 - 24.jpg", "../assets/img/bespoke/123 - 24.jpg", "../assets/img/bespoke/123 - 24.jpg"]
+        },
+        {
+            id: 'Bota-Chelsea-Marron',
+            titulo: 'Bota Chelsea',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Marrón',
+            tipoCuero: 'Vacuno',
+            imagenPrincipal: "../assets/img/bespoke/123 - 28.jpg",
+            imagenes: ["../assets/img/bespoke/123 - 28.jpg", "../assets/img/bespoke/123 - 28.jpg", "../assets/img/bespoke/123 - 28.jpg", "../assets/img/bespoke/123 - 28.jpg"]
+        },
+        {
+            id: 'Bota-Chelsea-Marron-Oscuro',
+            titulo: 'Bota Chelsea',
+            estilo: 'BESPOKE',
+            descripcion: 'Descripción del Sneaker.',
+            color: 'Marrón Oscuro',
+            tipoCuero: 'Vacuno',
+            imagenPrincipal: "../assets/img/bespoke/123 - 36.jpg",
+            imagenes: ["../assets/img/bespoke/123 - 36.jpg", "../assets/img/bespoke/123 - 36.jpg", "../assets/img/bespoke/123 - 36.jpg", "../assets/img/bespoke/123 - 36.jpg"]
+        }
+        
         // Aca puedo meter mas modelos
     ];
 
