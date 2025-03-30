cbc58542eb9199c50a637ef2f98dd9139fb7edfa
ansourian <jairansourian@gmail.com>
2025-03-30 12:55:05 -0300

    cambio boton article

--- js/main.js
+++ js/main.js
@@ -1,46 +1,59 @@
 function isElementInViewport(element) {
   const rect = element.getBoundingClientRect();
   return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
 
 function checkSections() {
-  const sections = document.querySelectorAll('.fade-section');
+  const sections = document.querySelectorAll(".fade-section");
 
   sections.forEach((section) => {
-      const rect = section.getBoundingClientRect();
-      const isVisible = rect.top <= window.innerHeight * 0.9;
+    const rect = section.getBoundingClientRect();
+    const isVisible = rect.top <= window.innerHeight * 0.9;
 
-      // Verificamos si el elemento está visible
-      if (isVisible) {
-          section.classList.add('visible'); // Añadimos la clase visible
-      } else {
-          section.classList.remove('visible'); // Eliminamos la clase visible
-      }
+    // Verificamos si el elemento está visible
+    if (isVisible) {
+      section.classList.add("visible"); // Añadimos la clase visible
+    } else {
+      section.classList.remove("visible"); // Eliminamos la clase visible
+    }
   });
 }
 
 // Ejecutamos checkSections cuando la página carga y en cada scroll
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   checkSections(); // Al cargar la página
 });
 
 let isScrolling = false;
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (!isScrolling) {
-      isScrolling = true;
-      requestAnimationFrame(() => {
-          checkSections();
-          isScrolling = false;
-      });
+    isScrolling = true;
+    requestAnimationFrame(() => {
+      checkSections();
+      isScrolling = false;
+    });
   }
 });
 
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const listaProductos = document.getElementById("lista-productos");
+
+  if (listaProductos) {
+    listaProductos.addEventListener("click", function (event) {
+      const producto = event.target.closest("article.producto");
+      if (producto) {
+        const productoId = producto.getAttribute("data-producto-id");
+        window.location.href = `modelo.html?productoId=${productoId}`;
+      }
+    });
+  }
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   const searchInput = document.getElementById("searchInput");
@@ -72,65 +85,64 @@ function scrollToTop() {
   let velocidad = 100;
 
   if (window.innerWidth < 768) {
-      velocidad = 800;
+    velocidad = 800;
   }
 
-  $('html, body').animate({scrollTop: 0}, velocidad);
+  $("html, body").animate({ scrollTop: 0 }, velocidad);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  const divAcercaDeTexts = document.querySelectorAll('.div-acerca_de_texts');
+document.addEventListener("DOMContentLoaded", function () {
+  const divAcercaDeTexts = document.querySelectorAll(".div-acerca_de_texts");
 
-  divAcercaDeTexts.forEach(div => {
-    div.addEventListener('mouseover', function() {
-      const img = div.parentElement.querySelector('.img-acerca_de');
-      img.classList.add('hovered');
+  divAcercaDeTexts.forEach((div) => {
+    div.addEventListener("mouseover", function () {
+      const img = div.parentElement.querySelector(".img-acerca_de");
+      img.classList.add("hovered");
     });
 
-    div.addEventListener('mouseout', function() {
-      const img = div.parentElement.querySelector('.img-acerca_de');
-      img.classList.remove('hovered');
+    div.addEventListener("mouseout", function () {
+      const img = div.parentElement.querySelector(".img-acerca_de");
+      img.classList.remove("hovered");
     });
   });
 });
 
 // Opcional: Detener la animación al pasar el cursor
-const marquee = document.querySelector('.marquee');
-marquee.addEventListener('mouseenter', () => {
-  marquee.querySelector('.marquee-content').style.animationPlayState = 'paused';
+const marquee = document.querySelector(".marquee");
+marquee.addEventListener("mouseenter", () => {
+  marquee.querySelector(".marquee-content").style.animationPlayState = "paused";
 });
-marquee.addEventListener('mouseleave', () => {
-  marquee.querySelector('.marquee-content').style.animationPlayState = 'running';
+marquee.addEventListener("mouseleave", () => {
+  marquee.querySelector(".marquee-content").style.animationPlayState =
+    "running";
 });
 
-const originalContent = marquee.querySelector('.marquee-content');
+const originalContent = marquee.querySelector(".marquee-content");
 
 function cloneContentInfinitely() {
-    const marqueeWidth = marquee.offsetWidth;
-    let totalWidth = 0;
+  const marqueeWidth = marquee.offsetWidth;
+  let totalWidth = 0;
 
-    while (totalWidth < marqueeWidth) {
-        const cloneContent = originalContent.cloneNode(true);
-        marquee.appendChild(cloneContent);
-        totalWidth += cloneContent.offsetWidth;
-    }
+  while (totalWidth < marqueeWidth) {
+    const cloneContent = originalContent.cloneNode(true);
+    marquee.appendChild(cloneContent);
+    totalWidth += cloneContent.offsetWidth;
+  }
 }
 
 cloneContentInfinitely();
 
-const marqueeContents = document.querySelectorAll('.marquee-content');
+const marqueeContents = document.querySelectorAll(".marquee-content");
 
 marqueeContents.forEach((marqueeContent) => {
-  marqueeContent.addEventListener('click', () => {
-    const isPaused = marqueeContent.getAttribute('data-paused') === 'true';
+  marqueeContent.addEventListener("click", () => {
+    const isPaused = marqueeContent.getAttribute("data-paused") === "true";
     if (isPaused) {
-      marqueeContent.style.animationPlayState = 'running';
-      marqueeContent.setAttribute('data-paused', 'false');
+      marqueeContent.style.animationPlayState = "running";
+      marqueeContent.setAttribute("data-paused", "false");
     } else {
-      marqueeContent.style.animationPlayState = 'paused';
-      marqueeContent.setAttribute('data-paused', 'true');
+      marqueeContent.style.animationPlayState = "paused";
+      marqueeContent.setAttribute("data-paused", "true");
     }
   });
 });
-
-
